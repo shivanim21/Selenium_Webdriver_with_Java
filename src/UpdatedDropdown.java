9f4d8d2383f758b14b051f971410f49bc29775d2
shivanim21 <shivanimaddala212@gmail.com>
2025-03-28 23:02:02 +0530

    dynamic dropdown added

--- src/UpdatedDropdown.java
+++ src/UpdatedDropdown.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class UpdatedDropdown {
 
@@ -19,6 +20,23 @@ public class UpdatedDropdown {
 //			i++;
 //		}
 //		driver.findElement(By.id("btnclosepaxoption")).click();
+			
+			System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+
+			for(int i=1;i<5;i++)
+
+			{
+
+			driver.findElement(By.id("hrefIncAdt")).click();
+
+			}
+
+
+			driver.findElement(By.id("btnclosepaxoption")).click();
+
+			Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
+
+			System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
 
 	}
 
